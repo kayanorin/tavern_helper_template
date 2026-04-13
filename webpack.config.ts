7e98b29b15bedb05e1dd2829e89fd450aa3795ae
kayanorin <kayanorin@gmail.com>
2026-04-13 13:36:47 +0800

    修改脚本

--- webpack.config.ts
+++ webpack.config.ts
@@ -540,7 +540,7 @@ function parse_configuration(entry: Entry): (_env: any, argv: any) => webpack.Co
 
       if (
         ['vue', 'vue-router'].every(key => request !== key) &&
-        ['pixi', 'react', 'vue'].some(key => request.includes(key))
+        ['pixi', 'react', 'vue', 'pinia'].some(key => request.includes(key))
       ) {
         return callback();
       }
